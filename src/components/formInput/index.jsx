59c7b8ebdd892331bdde03ea1fe9fe91b3a96a29
KrissKry <kryczka.krzysztof7@gmail.com>
2023-05-11 00:13:51 +0200

    added default size to text component, added comments and defaults to input

--- src/components/formInput/index.jsx
+++ src/components/formInput/index.jsx
@@ -1,22 +1,47 @@
 import './index.css'
 
-const FormInput = ({ type, name, value, onChange, placeholder, required, disabled, style }) => {
+/**
+ * @param type Type of input (ref. MDN Web Docs). DEFAULT="text"
+ * @param required Boolean if input is required. DEFAULT=false
+ * @param disabled Boolean if input should be disabled. DEFAULT=false
+ * @param className String for additional css classes. DEFAULT=""
+ * @param full If the input should have full width by design or half. DEFAULT=false 
+ * @param onKeyDown Function to react to input change (for example for text formatting of phone number)
+ * @returns FormInput component
+ */
+const FormInput = ({
+	type = "text",
+	name,
+	value,
+	onChange = function (){},
+	placeholder = "",
+	required = false,
+	disabled = false,
+	full = false,
+	className = "",
+	pattern,
+	onKeyDown = function (){},
+}) => {
 
-  const formStyle = style === 'full' ? 'formInput formInput-full' : 'formInput formInput-half';
+	const formStyle = full === true 
+		? 'formInput formInput-full' 
+		: 'formInput formInput-half' + ` ${className}`;
 
-  return (
-    <input
-      className={formStyle}
-      type={type}
-      id={name}
-      name={name}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      required={required}
-      disabled={disabled}
-    />
-  );
+	return (
+		<input
+		className={formStyle}
+		type={type}
+		id={name}
+		name={name}
+		value={value}
+		onChange={onChange}
+		placeholder={placeholder}
+		required={required}
+		disabled={disabled}
+		onKeyDown={onKeyDown}
+		pattern={pattern}
+		/>
+	);
 };
 
 export default FormInput;
