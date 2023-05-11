c5645689a9b57e5312e27e500755132d021705fd
KrissKry <kryczka.krzysztof7@gmail.com>
2023-05-11 18:33:56 +0200

    changed to button component. fixed css styling affecting button

--- src/sections/intro/index.jsx
+++ src/sections/intro/index.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 import Heading from '../../components/heading';
 import Text from '../../components/text';
+import Button from '../../components/button';
 
 const Intro = () => {
   return (
@@ -9,10 +10,11 @@ const Intro = () => {
         <Heading color="white" level={1} className="heading">
           CLOUDBUDGET
         </Heading>
-        <Text size='2.4rem' color="white" className="text">
+        <Text size='2.4rem' color="white" className="intro-text">
           Cloud budget management for everyone. Only €69.95 a Month After a 7 Day Trial of <br/>Up to €4.99
         </Text>
-        <a href="#" className="btn">SIGN UP</a>
+        {/* <a href="#" className="btn">SIGN UP</a> */}
+        <Button href="#" className="btn" text="sign up" uppercase secondary border={false} hover fit/>
       </div>
     </div>
   )
